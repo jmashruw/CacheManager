9d1c8e25a909461efc5cb6ce7396688e9a428e0b
Jinal Mashruwala <jmashruw@users.noreply.github.com>
2017-02-21 17:24:09 -0500

    Adding logic to handle modified data

--- src/Reader.java
+++ src/Reader.java
@@ -3,12 +3,15 @@
  */
 
 import java.util.concurrent.Callable;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Reader implements Callable<Integer> {
-    private Integer number;
+    public int number;
     private ReaderList myreader;
+    public boolean readCond = true;     //readCond to check if read needs to be done again
 
-    public Reader(Integer number, ReaderList myreader){
+    public Reader(int number, ReaderList myreader){
         this.number = number;
         this.myreader = myreader;
     }
@@ -16,11 +19,22 @@ public class Reader implements Callable<Integer> {
     @Override
     public Integer call() throws Exception{
         System.out.println("\n I am in read");
-        Boolean result = new Boolean(myreader.read(number));
-        if(result.equals(true))
-            System.out.println("\nNumber "+number + " found in the singly-linkedlist");
-        else
-            System.out.println("\nNumber "+number + " not found in the singly-linkedlist");
+
+        /*Repeat read if and only if item is modified*/
+        do{
+            ArrayList<Integer> result = myreader.read(number);
+            System.out.println("Numbers less than " + number + " are: ");
+
+            Iterator<Integer> resultIterator = result.iterator();
+            while (resultIterator.hasNext()) {
+                System.out.print(resultIterator.next() + " ");
+            }
+
+            if(myreader.modified == false)
+                readCond = false;
+
+        }while(readCond);
+
         return number;
     }
 }

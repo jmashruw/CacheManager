218dcd0d96499db8b8babf715afa21569e1fe994
Jinal Mashruwala <jmashruw@users.noreply.github.com>
2017-02-21 15:04:17 -0500

    Initial Commit

---
+++ src/Reader.java
@@ -0,0 +1,26 @@
+/**
+ * Created by Jiya on 2/21/17.
+ */
+
+import java.util.concurrent.Callable;
+
+public class Reader implements Callable<Integer> {
+    private Integer number;
+    private ReaderList myreader;
+
+    public Reader(Integer number, ReaderList myreader){
+        this.number = number;
+        this.myreader = myreader;
+    }
+
+    @Override
+    public Integer call() throws Exception{
+        System.out.println("\n I am in read");
+        Boolean result = new Boolean(myreader.search(number));
+        if(result.equals(true))
+            System.out.println("\nNumber "+number + " found in the singly-linkedlist");
+        else
+            System.out.println("\nNumber "+number + " not found in the singly-linkedlist");
+        return number;
+    }
+}

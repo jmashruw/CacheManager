d997af3750351a3d6cd9b3e4b02daa62813e1226
Jinal Mashruwala <jmashruw@users.noreply.github.com>
2017-02-21 17:26:25 -0500

    Implementing tester

--- src/Tester.java
+++ src/Tester.java
@@ -5,7 +5,6 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.ThreadPoolExecutor;
@@ -20,35 +19,25 @@ public class Tester {
 
             Random random = new Random();
 
-            Integer number = random.nextInt(10);
-            Writer k  = new Writer(number, temp);
-            Future<Integer> result = executor.submit(k);
-            resultList.add(result);
+            for (int i=0; i<10; i++) {
+                Integer number = random.nextInt(10);
+                Writer k = new Writer(number, temp);
+                Future<Integer> result = executor.submit(k);
+                resultList.add(result);
+            }
 
             for (int i=0; i<10; i++)
             {
-                number = random.nextInt(10);
+                Integer number = random.nextInt(10);
                 if(i%2 == 0){
                     Reader m  = new Reader(number, temp);
-                    result = executor.submit(m);
+                    Future<Integer> result = executor.submit(m);
                     resultList.add(result);
                 }
                 else{
-                    k  = new Writer(number, temp);
-                    result = executor.submit(k);
+                    Writer k  = new Writer(number, temp);
+                    Future<Integer> result = executor.submit(k);
                     resultList.add(result);
-                }
-            }
-
-            for(Future<Integer> future : resultList)
-            {
-                try
-                {
-                    System.out.println("Future result is - " + future.get());
-                }
-                catch (InterruptedException | ExecutionException e)
-                {
-                    e.printStackTrace();
                 }
             }
             //shut down the executor service now

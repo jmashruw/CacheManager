f21bde1f19818307dd4ba06d092036f1fb6b9980
Jinal Mashruwala <jmashruw@users.noreply.github.com>
2017-02-21 15:05:23 -0500

    Initial Commit

---
+++ src/Writer.java
@@ -0,0 +1,25 @@
+/**
+ * Created by Jiya on 2/21/17.
+ */
+
+import java.util.concurrent.Callable;
+
+public class Writer implements Callable<Integer>{
+
+        private Integer number;
+        private ReaderList mywriter;
+
+        public Writer(Integer number, ReaderList mywriter){
+            this.number = number;
+            this.mywriter = mywriter;
+        }
+
+        @Override
+        public Integer call() throws Exception{
+            System.out.println("\n I am in write");
+            mywriter.write(number);
+            System.out.println("\nNumber "+number + " written in the singly-linkedlist");
+            return number;
+        }
+    }
+}
